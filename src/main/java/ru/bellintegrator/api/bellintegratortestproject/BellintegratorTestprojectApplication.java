cc5fdf74023dedda8c415d0b38216fbfba4e6826
Timur Burganov <timk90@mail.ru>
2019-04-04 02:39:15 +0200

    initial commit

---
+++ src/main/java/ru/bellintegrator/api/bellintegratortestproject/BellintegratorTestprojectApplication.java
@@ -0,0 +1,13 @@
+package ru.bellintegrator.api.bellintegratortestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellintegratorTestprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellintegratorTestprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/bellintegrator/api/bellintegratortestproject/controller/Controller.java
@@ -0,0 +1,5 @@
+package ru.bellintegrator.api.bellintegratortestproject.controller;
+
+public class Controller {
+
+}
